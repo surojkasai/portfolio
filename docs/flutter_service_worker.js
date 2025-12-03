9ded601d969652ec2e1399e33d2dbc5698755cb0
surojkasai <surojkasai@gmail.com>
2025-12-03 13:52:05 +0545

    updated portfolio

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -34,7 +34,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "b16db6cea78dc2dfd1581181d0df8c92
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "ce32f125f9f70a34ebe2be62d1f40af8",
+"flutter_bootstrap.js": "552438f7047e358520589cb457c8e47f",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
